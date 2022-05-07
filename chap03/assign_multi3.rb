df5e967e91b7481ce472891798d55f2b5299792d
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-05-07 21:05:08 +0900

    3.2 代入演算子の学習完了

---
+++ chap03/assign_multi3.rb
@@ -0,0 +1,12 @@
+data = [1, 2, 3]
+
+x, y = data
+
+puts x
+puts y
+
+l, m, n, o = data
+puts l
+puts m
+puts n
+puts o
---
+++ chap03/ref_change2.rb
@@ -0,0 +1,6 @@
+data1 = [1, 2, 3]
+data2 = data1
+data1 = [4, 5, 6]
+
+p data1
+p data2
