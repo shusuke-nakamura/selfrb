8ab79ed1a68763af0a218d88e53be8d9fdef3005
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-06-26 08:02:51 +0900

    chap10の学習完了

---
+++ chap10/reserve_compare.rb
@@ -0,0 +1,37 @@
+class Coordinate
+  include Comparable
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def +(other)
+    case other
+    when Coordinate
+      Coordinate.new(self.x + other.x, self.y + other.y)
+    when Integer, Float
+      Coordinate.new(self.x + other, self.y + other)
+    else
+      raise TypeError, "type must be Coordinate or Integer, Float."
+    end
+  end
+
+  def <=>(other)
+    if other.instance_of?(Coordinate)
+      self.x ** 2 + self.y ** 2 <=> other.x ** 2 + other.y ** 2
+    end
+  end
+
+  def to_s
+    "(#{self.x}, #{self.y})"
+  end
+end
+
+c1 = Coordinate.new(10, 20)
+c2 = Coordinate.new(15, 25)
+c3 = Coordinate.new(20, 10)
+
+puts c1 > c2
+puts c1 == c3
